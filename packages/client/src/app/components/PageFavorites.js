dbf809a8c27131d1e8fd1eaa1274fa4e042a8a38
Jorge Ferreiro <jorge@ferreiro.me>
2019-09-05 00:22:15 +0200

    Added PageFavorites component

---
+++ packages/client/src/app/components/PageFavorites.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export function PageFavorites({
+    favorites,
+}) {
+    console.log('favorites')
+    console.log(favorites)
+    return (
+        <p>Page of the notifications</p>
+    )
+}
