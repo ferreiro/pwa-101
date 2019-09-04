979268e1b72d08e5d8fe25d22351e6bef6d4118b
Jorge Ferreiro <jorge@ferreiro.me>
2019-09-05 01:28:22 +0200

    Updating page artist with biography

--- packages/client/src/app/components/PageArtist.js
+++ packages/client/src/app/components/PageArtist.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { css } from 'emotion'
 
-
 const artistCardStyles = {
     wrapper: css``,
     image: css`
@@ -12,46 +11,6 @@ const artistCardStyles = {
     `,
 }
 
-function ArtistCard(props) {
-    const {
-        artist = {},
-        onFavoriteArtist,
-        onNotifyArtist,
-        purchaseUrl,
-    } = props;
-
-    const {
-        id,
-        name,
-        city,
-        country,
-        hero,
-        imageHero,
-    } = artist;
-
-    return (
-        <div className={artistCardStyles.wrapper}>
-            <Link to={`${PATH_ARTIST}/${id}`}>
-                
-            </Link>
-
-            <h3>📍 {city}</h3>
-
-            <ul>
-                <button onClick={onFavoriteArtist}>
-                    Save
-                </button>
-                <button onClick={onNotifyArtist}>
-                    Notify
-                </button>
-                <a href={purchaseUrl} target="_blank" rel="noopener noreferrer">
-                    RSVP
-                </a>
-            </ul>
-        </div>
-    )
-}
-
 export function PageArtist({
     artist,
 }) {
@@ -60,6 +19,7 @@ export function PageArtist({
         name,
         city,
         country,
+        biography,
         hero,
         imageHero,
     } = artist;
@@ -72,6 +32,11 @@ export function PageArtist({
                 alt={artist.name}
             />
             <h2>{name}</h2>
+            <p
+                dangerouslySetInnerHTML={{
+                    __html: biography
+                }}
+            />
         </span>
     )
 }
