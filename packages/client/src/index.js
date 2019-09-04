26bdfef4e2c745b52dd0fc379f6b8a14b5654e2f
Jorge Ferreiro <jorge@ferreiro.me>
2019-09-05 00:29:36 +0200

    Removed Notification render from index

--- packages/client/src/index.js
+++ packages/client/src/index.js
@@ -1,31 +1,18 @@
-function _objectDestructuringEmpty(obj) {
-    if (obj == null) throw new TypeError('Cannot destructure undefined')
-}
-
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import App from './app/App'
 
-var Notification = function Notification(_ref) {
-    _objectDestructuringEmpty(_ref)
-
-    return React.createElement('div', null, 'I\'m a notification LOL')
-}
-console.log('das');
-
 ReactDOM.render(
-    React.createElement(BrowserRouter, null, React.createElement(App, null)),
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
     document.getElementById('app')
 )
-ReactDOM.render(
-    React.createElement(Notification, null),
-    document.getElementById('notifications')
-)
 
-if (module.hot) {
-  module.hot.accept();
-  module.hot.dispose(function() {
-    clearInterval(timer);
-  });
-}
+// if (module.hot) {
+//     module.hot.accept()
+//     module.hot.dispose(function() {
+//           clearInterval(timer)
+//     })
+// }
