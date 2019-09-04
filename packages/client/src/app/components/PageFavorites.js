8add306403ae95bdf42ca0f30fc34647ae013c8f
Jorge Ferreiro <jorge@ferreiro.me>
2019-09-05 01:28:55 +0200

    Added empty state to PageFavorites

--- packages/client/src/app/components/PageFavorites.js
+++ packages/client/src/app/components/PageFavorites.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import isEmpty from 'lodash'
 
 export function PageFavorites({
     favorites,
 }) {
+    if (isEmpty(Object.keys(favorites))) {
+        return (
+            <p>You don't have any favorites... Go and add some!</p>
+        )
+    }
     console.log('favorites')
     console.log(favorites)
     return (
