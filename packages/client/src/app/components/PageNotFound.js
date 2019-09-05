16fe3bd980798d864aad3b43049c5e40e6aabc9a
Jorge Ferreiro <jorge@ferreiro.me>
2019-09-05 11:46:00 -0400

    Added PageNotFound

---
+++ packages/client/src/app/components/PageNotFound.js
@@ -0,0 +1,19 @@
+import React from 'react'
+// import isEmpty from 'lodash'
+// import { css } from 'emotion'
+// import { Link } from 'react-router-dom'
+
+// import { PATH_ARTIST } from '../constants/paths'
+// import { spacing1 } from '../constants/style'
+
+// TODO: We can pass some extra context here,
+// so we can notify the user how to recover from this error.
+export function PageNotFound({
+
+}) {
+    return (
+        <div>
+            Sorry! The page you are looking for does not exist...
+        </div>
+    )
+}
