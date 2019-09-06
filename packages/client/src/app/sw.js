dfc55a8d7a87460f0a1cebe43620db5ca9131e8f
Jorge Ferreiro <jorge@ferreiro.me>
2019-09-06 10:44:57 -0400

    ✅🎨 Added cache API to sw

--- packages/client/src/app/sw.js
+++ packages/client/src/app/sw.js
@@ -1,13 +1,38 @@
 'use strict'
 
-console.log('I am a new service worker :)')
+const cacheVersion = 'version-1'
+const criticalAssets = ['/client.bundle.js', '/index.html']
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(cacheAssets())
+})
+
+function cacheAssets() {
+    return caches.open(cacheVersion)
+        .then((cache) => {
+            cacheNoCriticalAssets(cache)
+
+            return cacheCriticalAssets(cache)
+        })
+}
+
+function cacheCriticalAssets (cache) {
+    return cache.addAll(criticalAssets)
+}
+
+function cacheNoCriticalAssets(cache) {
+    cache.add('./foo.css')
+}
 
 self.addEventListener('fetch', (event) => {
-    const url = event.request.url;
+    caches.open(cacheVersion).then((cache) => {
+        console.log('fetch::cache')
+        console.log(cache)
+    })
 
-    console.group('fetch')
-    console.log('event')
-    console.log(event)
-    console.log('url', url);
-    console.groupEnd()
+    console.log('I am intercepting network requests! ')
+
+    const url = event.request.url
+
+    console.log('Requested resource url: ', url)
 })
