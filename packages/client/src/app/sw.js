a6b6562932f3f6640fed92911aa027ec8e22e8d0
Jorge Ferreiro <jorge@ferreiro.me>
2019-09-06 14:21:42 -0400

    ✅ Yay! Implemented fetchImageOrFallback to placeholder with caching strategies

--- packages/client/src/app/sw.js
+++ packages/client/src/app/sw.js
@@ -1,7 +1,14 @@
 'use strict'
 
-const cacheVersion = 'version-2'
-const criticalAssets = ['/client.bundle.js', '/index.html']
+const cacheVersion = 'version-1'
+const PLADEHOLDER_IMAGE = '/images/placeholder.jpg'
+const criticalAssets = [
+    '/index.html',
+    '/client.bundle.js',
+    '/manifest.json',
+    PLADEHOLDER_IMAGE,
+    '/normalize.css'
+]
 
 self.addEventListener('install', (event) => {
     event.waitUntil(cacheAssets())
@@ -22,40 +29,106 @@ function cacheCriticalAssets (cache) {
 
 function cacheNoCriticalAssets(cache) {
     cache.add('./foo.css')
-    cache.add('./images/crystal_fighters.jpg')
+}
+
+function fetchHomePageOrFallback(fetchEvent) {
+    return caches.match('/index.html', {
+        cacheName: cacheVersion
+    })
+}
+
+function fetchManifestOrFallback(fetchEvent) {
+    return caches.match('/manifest.json', {
+        cacheName: cacheVersion
+    })
+}
+
+function fetchNormalizeOrFallback(fetchEvent) {
+    return caches.match('/normalize.css', {
+        cacheName: cacheVersion
+    })
+}
+
+function fetchJavascriptOrFallback(fetchEvent) {
+    return caches.match('/client.bundle.js', {
+        cacheName: cacheVersion
+    })
 }
 
 function fetchImageOrFallback(fetchEvent) {
-    return caches.match('./images/crystal_fighters.jpg', {
-        cacheName: cacheVersion
+    return fetch(fetchEvent.request, {
+        mode: 'no-cors'
     })
+        .then((response) => {
+            if (!response.ok) {
+                throw Error('Can not download image from server')
+            }
 
-    // return fetch(fetchEvent.request, {
-    //     mode: 'no-cors'
-    // })
-    // .then(({ response })) => {
-    //     if (!response.ok) {
-    //         return caches.match('')
-    //     }
-    // }
+            // Cache in Background the image
+            caches.open(cacheVersion).then(function(cache) {
+                cache.put(fetchEvent.request, response)
+            })
+
+            // We need to duplicate, otherwise it says the response was consumed twice
+            return response.clone()
+        })
+        .catch(() => {
+            // NB: Try to retun cached image...
+            return caches.match(event.request).catch(function() {
+                // NB: No cache image... Return placeholder
+                return caches.match(PLADEHOLDER_IMAGE, {
+                    cacheName: cacheVersion
+                })
+            })
+        })
 }
 
 self.addEventListener('fetch', (event) => {
     const acceptHeader = event.request.headers.get('accept')
     const requestUrl = new URL(event.request.url)
 
-    console.log('acceptHeader', acceptHeader)
-    console.log('requestUrl', requestUrl)
+    // console.log('acceptHeader', acceptHeader)
+    // console.log('requestUrl', requestUrl)
 
-    return event.respondWith(
-        fetchImageOrFallback(event)
-    )
-
-    if (acceptHeader.indexOf('image') === 0) {
-        if (requestUrl.pathname.indexOf('/images/') === 0) {
-            event.respondWith(
-                fetchImageOrFallback(event)
-            )
-        }
+    if (acceptHeader.includes('html')) {
+        return event.respondWith(
+            fetchHomePageOrFallback(event)
+        )
     }
+
+    if (requestUrl.pathname.includes('/manifest.json')) {
+        return event.respondWith(
+            fetchManifestOrFallback(event)
+        )
+    }
+
+    if (requestUrl.pathname.includes('normalize.css')) {
+        return event.respondWith(
+            fetchNormalizeOrFallback(event)
+        )
+    }
+
+    if (requestUrl.pathname.includes('/client.bundle.js')) {
+        return event.respondWith(
+            fetchJavascriptOrFallback(event)
+        )
+    }
+
+    if (acceptHeader.includes('image')) {
+        return event.respondWith(
+            fetchImageOrFallback(event)
+        )
+    }
+
+    // return event.respondWith(
+    //     fetchImageOrFallback(event)
+    // )
+
+    // if (acceptHeader.indexOf('image') === 0) {
+    //     if (requestUrl.pathname.indexOf('/images/') === 0) {
+    //         event.respondWith(
+    //             fetchImageOrFallback(event)
+    //         )
+    //     }
+    // }
 })
